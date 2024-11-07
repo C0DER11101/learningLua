9997c22063cfd7d2511f051cdab42aabb0be7dc5
C0DER11101 <while.alive.program@gmail.com>
2024-11-07 20:31:49 +0530

    prog2

--- expressions/programs/prog2.lua
+++ expressions/programs/prog2.lua
@@ -5,3 +5,5 @@ print(nil and 13);
 print(false and 13);
 print(4 or 5);
 print(false or 5);
+print(13 and false); -- also returns false because the first argument of the and operator is not 'false'
+print(15 or false); -- returns 15 because it's the first argument of the or operator and it's true
