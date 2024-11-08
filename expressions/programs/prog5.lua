6f7b2c70dfa682592ea351182caf5d0851d56aab
C0DER11101 <while.alive.program@gmail.com>
2024-11-08 19:23:04 +0530

    prog5

---
+++ expressions/programs/prog5.lua
@@ -0,0 +1,5 @@
+table = {math.sin(1), math.sin(2), math.sin(3), math.sin(4), math.sin(5), math.sin(6), math.sin(7), math.sin(8)}; -- a sine table
+
+print(table[1]);
+print(table[4]);
+print(table[7]);
