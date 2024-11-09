fb9c7387e03cd8ce1da0d36f558565fdaccd8250
C0DER11101 <while.alive.program@gmail.com>
2024-11-09 12:23:39 +0530

    prog2

--- statements/programs/prog2.lua
+++ statements/programs/prog2.lua
@@ -1,5 +1,13 @@
+-- list of values is shorter than list of variables
 a, b, c = 0, 1; -- a = 0, b = 1, c = nil
 
 print("a = ", a);
 print("b = ", b);
 print("c = ", c);
+
+-- list of values is longer than list of variables
+d, e, f = "hello", 10, 40, "world"; -- "world" is discarded
+
+print("d = ", d);
+print("d = ", e);
+print("d = ", f);
