3638a543df82bdb756f432abe034250b97bbfe4d
C0DER11101 <while.alive.program@gmail.com>
2024-11-09 12:25:24 +0530

    prog2

--- statements/programs/prog2.lua
+++ statements/programs/prog2.lua
@@ -11,3 +11,9 @@ d, e, f = "hello", 10, 40, "world"; -- "world" is discarded
 print("d = ", d);
 print("d = ", e);
 print("d = ", f);
+
+g, h, i = 0; -- i = 0, g = nil, h = nil
+
+print("d = ", g);
+print("d = ", h);
+print("d = ", i);
