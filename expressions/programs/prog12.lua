e765157e30e077fb01615699a131fc983efe3d11
C0DER11101 <while.alive.program@gmail.com>
2024-11-09 10:52:43 +0530

    prog12

---
+++ expressions/programs/prog12.lua
@@ -0,0 +1,9 @@
+a = {x = 10, y = 25; "one", "two", "three"}; -- separating the record-style and the list-style with a semicolon
+
+print(a.x); -- 10
+
+print(a["y"]); -- 25
+
+print(a[1]); -- "one"
+print(a[2]); -- "two"
+print(a[3]); -- "two"
