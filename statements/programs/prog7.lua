4987d53304dbbf7859c43ff816bdc95fd2298efc
C0DER11101 <while.alive.program@gmail.com>
2024-11-09 19:38:32 +0530

    pro7

---
+++ statements/programs/prog7.lua
@@ -0,0 +1,7 @@
+for i = 10, 1, -1 do -- step is -1, so it is going decrement i by 1
+	print(i);
+end
+
+for i = 1, 10 do -- no step specified, meaning that the step is +1 and i will be incremented by 1
+	print(i);
+end
