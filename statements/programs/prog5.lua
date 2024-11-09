e9393841fbff13d919cf1835caaa261e68633996
C0DER11101 <while.alive.program@gmail.com>
2024-11-09 15:15:22 +0530

    prog5

---
+++ statements/programs/prog5.lua
@@ -0,0 +1,15 @@
+op = "+";
+
+if op == "-" then
+	print(op);
+elseif op == "/" then
+	print(op);
+elseif op == "+" then
+	print(op);
+elseif op == "*" then
+	print(op);
+elseif op == "%" then
+	print(op);
+else
+	print("Invalid operator");
+end
