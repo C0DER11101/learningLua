3ae199286a8c7f7195db72dbfb2cdf62977fc2e8
C0DER11101 <while.alive.program@gmail.com>
2024-11-09 11:32:47 +0530

    prog1

---
+++ statements/programs/prog1.lua
@@ -0,0 +1,13 @@
+-- Example of multiple assignment
+
+a, b = 10, 20; -- a = 10 and b = 20
+
+print("a = " .. a);
+print("b = " .. b);
+
+-- swapping
+
+a, b = b, a;
+
+print("a = " .. a);
+print("b = " .. b);
