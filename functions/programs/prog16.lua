6022bb973ee21eb541b31e98d60d6bc1be2ddf4e
C0DER11101 <while.alive.program@gmail.com>
2024-11-11 19:44:13 +0530

    prog16

---
+++ functions/programs/prog16.lua
@@ -0,0 +1,7 @@
+print(select(1, 1, 2, 3));
+print(select(2, 1, 2, 3));
+print(select(3, 1, 2, 3));
+
+print(select(1, "Lua", "Programming", "Language"));
+print(select(2, "Lua", "Programming", "Language"));
+print(select(3, "Lua", "Programming", "Language"));
