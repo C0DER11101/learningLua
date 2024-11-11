6328d00653ee67fc3ea070168dd095f1543c9e46
C0DER11101 <while.alive.program@gmail.com>
2024-11-11 10:04:28 +0530

    prog3

--- functions/programs/prog3.lua
+++ functions/programs/prog3.lua
@@ -1,3 +1,3 @@
-s, e = string.find("Hello Lua users", "Lua");
+s, e = string.find("Hello Lua users", "Lua");  -- a multiple assignment
 
 print(s, e); -- prints 7 which is the starting index of `L' and 9 which is the ending index of `a' in the string 'Lua'
