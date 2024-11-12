89515ad56bc656f02c3cf3b7c2bedc625e0adc83
C0DER11101 <while.alive.program@gmail.com>
2024-11-12 10:09:35 +0530

    prog22

---
+++ functions/programs/prog22.lua
@@ -0,0 +1,18 @@
+network = {
+	{name = "grauna", IP = "210.26.30.34"},
+	{name = "arraial", IP = "210.26.30.23"},
+	{name = "lua", IP = "210.26.23.12"},
+	{name = "derain", IP = "210.26.23.20"}
+}
+
+for key in ipairs(network) do
+	print(network[key].name, network[key].IP);
+end
+
+table.sort(network, function(a, b) return a.name < b.name end)
+
+print"-----------------------------"
+
+for key in ipairs(network) do
+	print(network[key].name, network[key].IP);
+end
