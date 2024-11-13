47deff1cba6bbbf21e3f33aac1023550c4c2a9ea
C0DER11101 <while.alive.program@gmail.com>
2024-11-13 16:17:40 +0530

    prog35

---
+++ functions/programs/prog35.lua
@@ -0,0 +1,30 @@
+function room1()
+	local move = io.read()
+	if move == "south" then return room3()
+	elseif move == "east" then return room2()
+	else print("Invalid move")
+		return room1()
+	end
+end
+
+function room2()
+	local move = io.read()
+	if move == "south" then return room4()
+	elseif move == "west" then return room1()
+	else print("invalid move")
+	end
+end
+
+function room3()
+	local move = io.read()
+	if move == "south" then return room1()
+	elseif move == "east" then return room4()
+	else print("invalid move")
+	end
+end
+
+function room4()
+	print("Congratulations!")
+end
+
+room1()
