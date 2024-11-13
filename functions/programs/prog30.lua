e024f7a82b3d20225f14ea5eb7da2b8eaddeeb86
C0DER11101 <while.alive.program@gmail.com>
2024-11-13 10:57:13 +0530

    prog30

---
+++ functions/programs/prog30.lua
@@ -0,0 +1,20 @@
+maths = {}
+
+maths.add = function (x, y) return x + y end
+maths.sub = function (x, y) return x - y end
+maths["mul"] = function (x, y) return x * y end
+
+
+function maths.div(x, y)
+	if y == 0 then
+		print("Divide by 0!");
+		return nil
+	end
+	return x/y;
+end
+
+print(maths.add(10, 20));
+print(maths.sub(10, 20));
+print(maths.mul(10, 20));
+print(maths.div(12, 20));
+print(maths.div(12, 0));
