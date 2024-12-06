4e3de1dc2771b23d14f3da3fcdd142a002cb6128
C0DER11101 <g00fc0der@yahoo.com>
2024-12-06 13:54:51 +0530

    prog4

--- iterators_and_generic_for/programs/prog4.lua
+++ iterators_and_generic_for/programs/prog4.lua
@@ -1,4 +1,4 @@
 print "before table"
-state = {line = io.read(), pos = 1}
+state = {line = io.read(), pos = 1} -- take user input here
 print "after table"
-print(state.line)
+print(state.line) -- print the user input here
