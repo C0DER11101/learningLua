0baa93c818dc4886f3494f6b8c04450f326d679a
C0DER11101 <g00fc0der@yahoo.com>
2025-02-01 17:21:46 +0530

    prog10

--- compilation_execution_errors/programs/prog10.lua
+++ compilation_execution_errors/programs/prog10.lua
@@ -1,3 +1,5 @@
+-- Program taken from Programming in Lua: Chapter 8
+
 n = io.read()
 
 assert(tonumber(n), "invalid input: " .. n .. " is not a number") -- Lua will always do the concatenation of n with this string even when n is a number
